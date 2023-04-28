d9c2279eda0c1cc71121a1e609296d0920027ade
AdhiNandanJ <adhinandanj@gmail.com>
2023-04-29 03:59:54 +0530

    Completely remove "attendance.txt"

--- main.go
+++ main.go
@@ -3,72 +3,69 @@ package main
 import (
 	"bufio"
 	"context"
-	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Funtion for logging errors
+// Function for logging errors
 func errorHandler(err error) {
 	println("Ops, something went wrong:", err)
 }
 
-//Function to get attendance
+// Function to get attendance
+func viewAttendance(collection *mongo.Collection) {
+	fmt.Println()
+	fmt.Println("This will show you attendance")
 
-func viewAttendance() {
-	println()
-	println("This will show you attendance")
+	cur, err := collection.Find(context.Background(), nil)
+	if err != nil {
+		errorHandler(err)
+		log.Fatal(err)
+	}
+	defer cur.Close(context.Background())
 
-	if _, err := os.Stat("attendance.txt"); os.IsNotExist(err) {
-		println("No attendance to show")
-		println()
-	} else {
-		file, err := os.Open("attendance.txt")
+	if cur.Err() != nil {
+		errorHandler(err)
+		log.Fatal(err)
+	}
+
+	if cur.RemainingBatchLength() == 0 {
+		fmt.Println("No attendance to show")
+		fmt.Println()
+		return
+	}
+
+	for cur.Next(context.Background()) {
+		var result map[string]string
+		err := cur.Decode(&result)
 		if err != nil {
 			errorHandler(err)
 			log.Fatal(err)
 		}
-		defer file.Close()
-
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-		}
-
-		if err := scanner.Err(); err != nil {
-			errorHandler(err)
-			log.Fatal(err)
-		}
-
+		fmt.Println(result)
 	}
 
+	if err := cur.Err(); err != nil {
+		errorHandler(err)
+		log.Fatal(err)
+	}
 }
 
 func resetAttendance(collection *mongo.Collection) {
-	println("This will reset attendance")
-
-	if _, err := os.Stat("attendance.txt"); os.IsNotExist(err) {
-		println("Attendance already clear")
-	} else {
-		e := os.Remove("attendance.txt")
-		if e != nil {
-			errorHandler(e)
-			log.Fatal(e)
-		} else {
-			err := collection.Drop(context.TODO())
-			if err != nil {
-				errorHandler(err)
-				log.Fatal(err)
-			}
-			println("Attendance Collection Dropped")
-		}
+	fmt.Println("This will reset attendance")
+	err := collection.Drop(context.Background())
+	if err != nil {
+		errorHandler(err)
+		log.Fatal(err)
 	}
+	fmt.Println("Attendance Collection Dropped")
 }
 
 func getStudentInfo() (normtime, epochtime, name, roll, course string) {
@@ -94,7 +91,6 @@ func getStudentInfo() (normtime, epochtime, name, roll, course string) {
 	course, _ = inputReader.ReadString('\n')
 
 	return strings.TrimSpace(normtime), strings.TrimSpace(epochtime), strings.TrimSpace(name), strings.TrimSpace(roll), strings.TrimSpace(course)
-
 }
 
 // Main
@@ -103,10 +99,10 @@ func main() {
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatal("Failed to connect to MongoDB", err)
-
 	}
 	defer client.Disconnect(context.TODO())
 	collection := client.Database("Attendance").Collection("attendance")
+
 	for {
 		fmt.Println()
 		fmt.Println("===========================")
@@ -117,17 +113,38 @@ func main() {
 		fmt.Println("4 to exit")
 		fmt.Println("===========================")
 		fmt.Println()
-		var option int
 
+		var option int
 		fmt.Scanln(&option)
 
 		switch option {
 		case 1:
-			viewAttendance()
+			// View attendance from MongoDB
+			cur, err := collection.Find(context.Background(), bson.M{})
+			if err != nil {
+				errorHandler(err)
+				log.Fatal(err)
+			}
+			defer cur.Close(context.Background())
+
+			for cur.Next(context.Background()) {
+				var result bson.M
+				err := cur.Decode(&result)
+				if err != nil {
+					errorHandler(err)
+					log.Fatal(err)
+				}
+				fmt.Println(result["normtime"], result["name"], result["roll"], result["course"])
+			}
+			if err := cur.Err(); err != nil {
+				errorHandler(err)
+				log.Fatal(err)
+			}
 		case 2:
+			// Log attendance to MongoDB
 			normtime, epochtime, name, roll, course := getStudentInfo()
-			record := []string{normtime, epochtime, name, roll, course}
-			_, err := collection.InsertOne(context.TODO(), map[string]string{
+
+			res, err := collection.InsertOne(context.TODO(), bson.M{
 				"normtime":  normtime,
 				"epochtime": epochtime,
 				"name":      name,
@@ -138,30 +155,20 @@ func main() {
 				errorHandler(err)
 				log.Fatal(err)
 			}
-			file, err := os.OpenFile("attendance.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-			if err != nil {
-				panic(err)
-			}
-
-			w := csv.NewWriter(file)
-
-			defer file.Close()
-
-			w.Write(record)
-			w.Flush()
-			err = w.Error()
-
+			fmt.Println("Attendance added successfully with ID:", res.InsertedID)
+		case 3:
+			// Reset attendance collection
+			err := collection.Drop(context.Background())
 			if err != nil {
 				errorHandler(err)
-				log.Fatalf("%s", err)
+				log.Fatal(err)
 			}
-		case 3:
-			resetAttendance(collection)
+			fmt.Println("Attendance Collection Dropped")
 		case 4:
-			fmt.Println("Exiting the program...")
+			fmt.Println("Exited the program...")
 			return
 		case -1:
-			fmt.Println("Exiting the program...")
+			fmt.Println("Exited the program...")
 			return
 		default:
 			fmt.Println("Invalid option")
